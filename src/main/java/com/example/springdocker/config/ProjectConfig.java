a19a202087ae770bba9a1f89e464987e3108bb1c
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-02-17 19:57:45 +0100

    chore: add modelmapper configuration

---
+++ src/main/java/com/example/springdocker/config/ProjectConfig.java
@@ -0,0 +1,17 @@
+package com.example.springdocker.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+
+        return modelMapper;
+    }
+}
