61480313c4cf4941d9e1847ad983a487b02bad93
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-02-17 19:42:32 +0100

    chore: add employee dto classes

---
+++ src/main/java/com/example/springdocker/dto/request/AddEmployeeRequest.java
@@ -0,0 +1,40 @@
+package com.example.springdocker.dto.request;
+
+public class AddEmployeeRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String department;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
